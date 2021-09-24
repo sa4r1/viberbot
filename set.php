5fa4ba1d42d283683150d284b1a37cb42ff349d1
sa4r1 <notenterddns@gmail.com>
2021-09-25 00:25:43 +0300

    1

---
+++ set.php
@@ -0,0 +1,21 @@
+<?php
+	$auth_token = '4e01c7364b67e33a-b9f3db0adbf60360-590941c35d4d41a7';
+	$webhook = 'WEBHOOK_URL';
+	
+	$jsonData = 
+	'{
+		"auth_token": "'.$auth_token.'",
+		"url": "'.$webhook.'",
+		"event_types": ["subscribed", "unsubscribed", "delivered", "message", "seen"]
+	}';
+	
+	$ch = curl_init('https://chatapi.viber.com/pa/set_webhook');
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+	$response = curl_exec($ch);
+	$err = curl_error($ch);
+	curl_close($ch);
+	if($err) {echo($err);}
+	else {echo($response);}
+?>
