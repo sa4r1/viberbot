57838f3369a360d46174b0605aab0ee974ee0109
sa4r1 <notenterddns@gmail.com>
2021-09-25 01:34:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -34,6 +34,112 @@ function sendReq($data)
 	else {return $response;}
 }
 
+function getMainMenu($user_id){
+	$data['auth_token'] = $auth_token;
+	$data['receiver']=$user_id;
+  $data['BgColor']="#ffffff";
+  $data['type']='text';
+  $data['text']="Please select one of the options below:";
+  $keyboard_array['Type']='keyboard';
+  $keyboard_array['DefaultHeight']=false;
+  $keyboard_array['BgColor']="#FFFFFF";
+  $keyboard['keyboard']=$keyboard_array;
+ 
+  $news['Columns']=2;
+  $news['Rows']=2;
+  $news['TextVAlign']="bottom";
+  $news['TextHAlign']="center";
+  $news['TextOpacity']="100";
+  $news['Text']='NEWS';
+  $news['TextSize']="regular";
+  $news['ActionType']="reply";
+  $news['ActionBody']="LCNews";
+  $keyboard['keyboard']['Buttons'][]=$news;
+ 
+ 
+  $articles['Columns']=2;
+  $articles['Rows']=2;
+  $articles['TextVAlign']="bottom";
+  $articles['TextHAlign']="center";
+  $articles['TextOpacity']="100";
+  $articles['Text']='ARTICLES';
+  $articles['TextSize']="regular";
+  $articles['ActionType']="reply";
+  $articles['ActionBody']="LCArticles";
+  $keyboard['keyboard']['Buttons'][]=$articles;
+ 
+ 
+  $interviews['Columns']=2;
+  $interviews['Rows']=2;
+  $interviews['TextVAlign']="bottom";
+  $interviews['TextHAlign']="center";
+  $interviews['TextOpacity']="100";
+  $interviews['Text']='INTERVIEWS';
+  $interviews['TextSize']="regular";
+  $interviews['ActionType']="reply";
+  $interviews['ActionBody']="LCInterviews";
+  $keyboard['keyboard']['Buttons'][]=$interviews;
+ 
+  $gallery['Columns']=2;
+  $gallery['Rows']=2;
+  $gallery['TextVAlign']="bottom";
+  $gallery['TextHAlign']="center";
+  $gallery['TextOpacity']="100";
+  $gallery['Text']='GALLERY';
+  $gallery['TextSize']="regular";
+  $gallery['ActionType']="reply";
+  $gallery['ActionBody']="LCGallery";
+  $keyboard['keyboard']['Buttons'][]=$gallery; 
+ 
+  $poll['Columns']=2;
+  $poll['Rows']=2;
+  $poll['TextVAlign']="bottom";
+  $poll['TextHAlign']="center";
+  $poll['TextOpacity']="100";
+  $poll['Text']='POLL';
+  $poll['TextSize']="regular";
+  $poll['ActionType']="reply";
+  $poll['ActionBody']="LCPoll";
+  $keyboard['keyboard']['Buttons'][]=$poll;
+ 
+  $potm['Columns']=2;
+  $potm['Rows']=2;
+  $potm['TextVAlign']="bottom";
+  $potm['TextHAlign']="center";
+  $potm['TextOpacity']="100";
+  $potm['Text']='PLAYER OF THE MONTH';
+  $potm['TextSize']="regular";
+  $potm['ActionType']="reply";
+  $potm['ActionBody']="LCPOTM";
+  $keyboard['keyboard']['Buttons'][]=$potm;
+ 
+  $quote['Columns']=2;
+  $quote['Rows']=2;
+  $quote['TextVAlign']="bottom";
+  $quote['TextHAlign']="center";
+  $quote['TextOpacity']="100";
+  $quote['Text']='QUOTE OF THE DAY';
+  $quote['TextSize']="regular";
+  $quote['ActionType']="reply";
+  $quote['ActionBody']="LCQuote";
+  $keyboard['keyboard']['Buttons'][]=$quote;
+ 
+  $website['Columns']=4;
+  $website['Rows']=2;
+  $website['TextVAlign']="bottom";
+  $website['TextHAlign']="center";
+  $website['TextOpacity']="100";
+  $website['Text']='VISITE OUR WEBSITE';
+  $website['TextSize']="regular";
+  $website['ActionType']="open-url";
+  $website['ActionBody']="http://www.letzcricket.com";
+  $keyboard['keyboard']['Buttons'][]=$website;
+ 
+  $data['keyboard']=$keyboard['keyboard'];
+
+  return sendReq($data);
+}
+
 function sendMsg($sender_id, $text, $type, $tracking_data = Null, $arr_asoc = Null)
 {
 	global $auth_token, $send_name;
@@ -69,6 +175,7 @@ $sender_id = $input['sender']['id']; //unique viber id of user who sent the mess
 $sender_name = $input['sender']['name']; //name of the user who sent the message
 
 
+
 if($input['event'] == 'webhook') 
 {
   $webhook_response['status'] = 0;
@@ -92,6 +199,7 @@ else if($input['event'] == "conversation_started")
 }
 elseif($input['event'] == "message")
 {
+	getMainMenu($sender_id);
   sendMsg($sender_id, $text, $type);
 }
 
